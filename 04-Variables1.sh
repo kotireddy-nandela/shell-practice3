bc751b2e70d6e58c76e98d6cef45f01d1eb47203
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-28 19:05:45 +0530

    Varaibles

---
+++ 04-Variables1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1 #no space between value and =
+PERSON2=$2
+
+echo "$PERSON1:: Hello PERSON2, How are you?"
+echo "$PERSON2:: I am fine thank you, HOw about you?"
+echo "$PERSON1:: I am good, What are you learing"
+echo "$PERSON2:: I am learing DEVOPS with AWS and Shell"
+echo "$PERSON1:: Oh super  i am aslo learing  Shell"
+echo "$PERSON2:: excelent, Good topic"
