8d67aa30631e978e3c1e3643ecb03a5cab4807c4
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-28 18:54:56 +0530

    Variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-PERSON1=Kotireddy #no space between value and =
-PERSON2=Priya
+PERSON1=Srinu #no space between value and =
+PERSON2=Reddy
 
 echo "$PERSON1:: Hello PERSON2, How are you?"
 echo "$PERSON2:: I am fine thank you, HOw about you?"
 echo "$PERSON1:: I am good, What are you learing"
 echo "$PERSON2:: I am learing DEVOPS with AWS and Shell"
 echo "$PERSON1:: Oh super  i am aslo learing  Shell"
+echo "$PERSON2:: excelent, Good topic
