568ae4248ff2c898f37393fdefb055fa2c8884b4
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-28 18:32:18 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Kotireddy:: Hello priaya, How are you?"
+echo "Priya:: I am fine thank you, HOw about you?"
+echo "Kotireddy:: I am good, What are you learing"
+echo "Priya:: I am learing DEVOPS with AWS and Shell"
+echo "Kotireddy:: Oh super  i am aslo learing  Shell"
