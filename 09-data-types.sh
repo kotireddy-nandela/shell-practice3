d4d4b472306626d246e4dd826df56d372e63dd1c
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-29 17:32:09 +0530

    Special variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -7,6 +7,14 @@ SUM=$((NUM1+NUM2))
 
 echo "Sum is  : $SUM"
 
+#ARRAYS
+
 FRUITS=("APPLE" "BANANA" "PROMO")
 
 echo "Fruits are :${FRUITS[@]}"
+echo "Fruits Deatils orderdby"
+echo "First fruit is :${FRUITS[0]}"
+echo " Second fruit is :${FRUITS[1]}"
+echo "Third fruit is : ${FRUITS[2]}"
+
+
