42067a609b9f9d4c78513f3ce76a97ae9f9d0fb6
dawsvenkat <kotireddyn2688@gmail.com>
2026-01-29 17:49:01 +0530

    Conditions

---
+++ 10-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt = Greater than
+# -lt = Less than
+# -eq = equal
+# -ne = Not equal
+
+if [ $NUMBER -gt 25 ]; then
+
+   echo "Given number  :$NUMBER is greater than 25"
+
+fi   
+
